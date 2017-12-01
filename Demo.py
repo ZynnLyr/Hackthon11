b1755cedc20f3fe0f85d95fc9c92cd657cb747fa
Vendi <stereocn>
2017-12-01 14:29:21 +1100

    higher level access token

--- Demo.py
+++ Demo.py
@@ -1,8 +1,7 @@
-from flask import Flask
 import requests
 
-access_token = 'xeoK0YBRkqO5US8JdBXAZQp1rTSfUq'
-refresh_token = 'JCwmyJpvcQPiEqqJ8THdHOToQiqTeo'
+access_token = 'JwRmvUjaVL3Qfr6x8ifWsRTpPFDVc3'
+refresh_token = 'xFOTwK0p6jULSuRXTdwo6muBd7WLec'
 
 # Users who hit this endpoint will be redirected to the authorisation prompt
 
@@ -14,3 +13,4 @@ oauth_headers = {"Freelancer-OAuth-V1": access_token}
 
 response = requests.get(url, headers=oauth_headers)
 print(response.content)
+
